d2931013fda5f07f75a175db2d5c87b7e87e982d
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2022-01-30 01:50:00 +0100

    Form database model addition

---
+++ DFMS.Database/Models/DbForm.cs
@@ -0,0 +1,10 @@
+﻿using DFMS.Database.Models.Base;
+
+namespace DFMS.Database.Models
+{
+    public class DbForm : DbBaseModel
+    {
+        public virtual string ReferenceNumber { get; set; }
+        public virtual DbFormTemplate Template { get; set; }
+    }
+}
