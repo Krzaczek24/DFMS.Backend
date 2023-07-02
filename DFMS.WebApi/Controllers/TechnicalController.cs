c2cdf5a22eed7134ad5eaa892aeb9c49a778e148
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2023-07-02 17:11:24 +0200

    TechnicalController added with Ping method

---
+++ DFMS.WebApi/Controllers/TechnicalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+namespace DFMS.WebApi.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    public class TechnicalController
+    {
+        public TechnicalController() { }
+
+        [HttpPost("/ping")]
+        public void Ping() { Thread.Sleep(100); }
+    }
+}
