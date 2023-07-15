bba77381779e275daa051a8247e1b4ae2b495d5c
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2023-07-15 23:24:59 +0200

    Added automatic frontend starting

--- DFMS.WebApi/Program.cs
+++ DFMS.WebApi/Program.cs
@@ -4,23 +4,40 @@ using Microsoft.Extensions.Logging;
 using NLog;
 using NLog.Web;
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace DFMS.WebApi
 {
     public class Program
     {
+        private static Logger Logger { get; set; }
+
         public static void Main(string[] args)
         {
-            var logger = LogManager.Setup().LoadConfigurationFromFile().GetCurrentClassLogger();
+            Logger = LogManager.Setup().LoadConfigurationFromFile().GetCurrentClassLogger();
 
             try
             {
-                logger.Info("init main");
+#if DEBUG
+                const int frontendPort = 3000;
+                if (GetProcessIdByPort(frontendPort).HasValue)
+                {
+                    Logger.Info($"> React Frontend already running on [{frontendPort}] port");
+                }
+                else
+                {
+                    Logger.Info("> Starting React Frontend");
+                    RunFrontendProcess();
+                }
+#endif
+                Logger.Info("> Starting WebApi");
                 CreateHostBuilder(args).UseNLog().Build().Run();
             }
             catch (Exception ex)
             {
-                logger.Error(ex, "Stopped program because of exception");
+                Logger.Error(ex, "Stopped program because of exception");
                 throw;
             }
             finally
@@ -38,5 +55,50 @@ namespace DFMS.WebApi
                     webBuilder.UseNLog();
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static int? GetProcessIdByPort(int port)
+        {
+            string? output;
+            var proc = new Process()
+            {
+                StartInfo = new ProcessStartInfo("netstat", "-ano")
+                {
+                    RedirectStandardOutput = true
+                }
+            };
+
+            proc.Start();
+            while (!(output = proc.StandardOutput.ReadLine())?.Contains($"0.0.0.0:{port}") ?? false) { }
+            proc.Kill();
+
+            if (string.IsNullOrEmpty(output))
+                return null;
+
+            output = output.Split(' ', StringSplitOptions.RemoveEmptyEntries).Last();
+
+            if (int.TryParse(output, out int processId))
+                return processId;
+
+            return null;
+        }
+
+        private static void RunFrontendProcess()
+        {
+            var frontentStartBat = new FileInfo(@"..\..\DFMS.Frontend.TypeScriptReactWithMobx\run-frontend.bat");
+            if (frontentStartBat.Exists)
+            {
+                new Process()
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        WorkingDirectory = frontentStartBat.DirectoryName,
+                        FileName = "cmd",
+                        Arguments = $"/c {frontentStartBat.FullName}",
+                        UseShellExecute = true,
+                        CreateNoWindow = false,
+                    }
+                }.Start();
+            }
+        }
     }
 }
